bc0c98813f7e2ff9707dd0614b250ac456651a15
Elminson De Oleo Baez <edeoleo@gmail.com>
2023-12-06 22:56:54 -0500

    Add GetMyIP project with reqwest and async-std dependencies

---
+++ src/request.rs
@@ -0,0 +1,21 @@
+use reqwest;
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    // Specify the URL you want to make a request to
+    let url = "https://httpbin.org/ip";
+
+    // Make the HTTP GET request
+    let response = reqwest::get(url).await?;
+
+    // Check if the request was successful (HTTP status code 200 OK)
+    if response.status().is_success() {
+        // Print the response body as a string
+        let body = response.text().await?;
+        println!("Response body: {}", body);
+    } else {
+        println!("Request failed with status code: {}", response.status());
+    }
+
+    Ok(())
+}
